0f87b18b3cae0a05160e8d77e9aa4f3e8b43ae4a
shunGoes <shunnode@gmail.com>
2024-02-20 17:34:48 +0100

    added total to checkout

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,21 @@
 import {useContext} from 'react'
 import {CartContext} from '../../context/cart.context'
 
+import {useNavigate} from 'react-router-dom'
+
 import './cart-dropdown.styles.scss'
 
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
 
 const CartDropdown = () => {
+  const navigate = useNavigate()
+
   const {cartItems} = useContext(CartContext)
+
+  function goToCheckoutHandler(){
+    navigate('/checkout')
+  }
   return (
     <div className="cart-dropdown-container">
         <div className="cart-items">
@@ -15,7 +23,7 @@ const CartDropdown = () => {
             cartItems.map(item => <CartItem key={item.id} cartItem={item} />)
           }
           </div>
-        <Button>GO TO CHECKOUT</Button>
+        <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   )
 }
